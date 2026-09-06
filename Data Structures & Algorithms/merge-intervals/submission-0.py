47e109e6ee77ec26367a81e1cc08a717097a3d6c
! N1ghtm4r3e <ayoube_akhrif2015@outlook.fr>
2026-09-06 10:59:42 +0100

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        print(intervals)
+        res = []
+        prev = intervals[0]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= prev[1]:
+                prev[1] = max(intervals[i][1], prev[1])
+            else:
+                res.append(prev)
+                prev = intervals[i]
+        res.append(prev)
+        return res
